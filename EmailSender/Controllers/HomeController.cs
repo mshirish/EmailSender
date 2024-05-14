8277ae09bcaec3f863a13d7081fb3b5c39d55365
mshirish <104812313+mshirish@users.noreply.github.com>
2024-05-14 09:16:15 +0545

    Feat: Email Sender

---
+++ EmailSender/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using EmailSender.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+    
+        public HomeController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var receiver = "manishstha44@gmail.com";
+            var subject = "Email testing";
+            var message = "Hello World";
+
+            await _emailSender.SendEmailAsync(receiver, subject, message);
+
+            return View();
+
+        }
+    }
+
+}
---
+++ EmailSender/EmailSenderClass.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender
+{
+    public class EmailSenderClass : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "mshirish2002@gmail.com";
+            var pw = ""; //replace this part with email password
+
+            var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pw)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: mail,
+                                to: email,
+                                subject,
+                                message));
+        }
+    }
+}
